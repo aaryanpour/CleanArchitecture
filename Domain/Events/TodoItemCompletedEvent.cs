f704444f445b83db9dc464a97ede94df64447ccf
Abbas Aryanpour <abbasaryanpour@gmail.com>
2022-05-30 10:48:49 +0430

    Add project files.

---
+++ Domain/Events/TodoItemCompletedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Events;
+public class TodoItemCompletedEvent : BaseEvent
+{
+    public TodoItemCompletedEvent(TodoItem item)
+    {
+        Item = item;
+    }
+
+    public TodoItem Item { get; }
+}
